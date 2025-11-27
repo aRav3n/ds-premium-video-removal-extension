9d6b2cfdb24c6c553c8e3de0187263809a3ba13b
aRav3n <24660772+aRav3n@users.noreply.github.com>
2025-11-27 15:45:15 +0530

    build: get the gridParent element from the DOM

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ds-premium-video-removal-extension
+
+
+Credit for logo: https://pictogrammers.com/library/mdi/icon/broom/
--- content_script.js
+++ content_script.js
@@ -1,1 +1,49 @@
-// Put all the javascript code here, that you want to execute after page load.
+function placeIntoParentDiv(childDiv) {
+  const newParentDiv = document.querySelector(
+    "div.ds-browse-page__videos > div.ds-browse-videos"
+  );
+}
+
+function deletePremiumVideos() {
+  const allVideoDivs = document.querySelectorAll(".ds-browse-videos__video");
+  for (let videoDiv of allVideoDivs) {
+    const premiumLink = videoDiv.querySelector('a[href="/premium"]');
+    if (premiumLink) {
+      videoDiv.remove();
+    } else {
+    }
+  }
+}
+
+function setGridParent() {
+  let gridParent;
+
+  if (!gridParent) {
+    const parent = document.querySelector(
+      "#root > .ds-app > .ds-content-area > .ds-scrollable-area > .ds-page.ds-browse-page > .ds-browse-page__videos > .ds-browse-videos"
+    );
+    if (parent && parent !== undefined) {
+      gridParent = parent;
+      gridParent.setAttribute("id", "grid-parent");
+      return gridParent;
+    }
+  }
+  return null;
+}
+
+function runCode() {
+  const gridParent = setGridParent();
+  if (gridParent) {
+    console.log(gridParent);
+  }
+  // deletePremiumVideos();
+  // rearrangeVideos();
+}
+
+window.addEventListener("load", () => {
+  runCode();
+});
+
+window.addEventListener("change", () => {
+  runCode();
+});
